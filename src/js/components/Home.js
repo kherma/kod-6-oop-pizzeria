2368b0771c14f013672b379a7d9fde6643832125
kherma <kamil.herma01@gmail.com>
2025-11-19 00:25:42 +0100

    feat: add to home page

--- src/js/components/Home.js
+++ src/js/components/Home.js
@@ -5,6 +5,7 @@ class Home {
     const thisHome = this;
     thisHome.render(element);
     thisHome.hideElements();
+    thisHome.slider();
   }
 
   hideElements() {
@@ -27,6 +28,50 @@ class Home {
 
     thisHome.dom.wrapper = element;
     thisHome.dom.wrapper.innerHTML = generatedHTML;
+  }
+
+  slider() {
+    const thisHome = this;
+    thisHome.currentSlide = 0;
+    thisHome.slides = document.querySelectorAll('.slide');
+    const maxSlides = thisHome.slides.length;
+
+    // Infinite slider
+    setInterval(() => {
+      if (thisHome.currentSlide === maxSlides - 1) {
+        thisHome.currentSlide = -1;
+      }
+
+      thisHome.currentSlide++;
+      thisHome.goToSlide(thisHome.currentSlide);
+      thisHome.activeDot(thisHome.currentSlide);
+    }, 3000);
+
+    // Change slide on click
+    document.querySelector('.dots').addEventListener('click', function (event) {
+      if (event.target.classList.contains('dot')) {
+        thisHome.currentSlide = Number(event.target.dataset.slide);
+        thisHome.activeDot(thisHome.currentSlide);
+        thisHome.goToSlide(thisHome.currentSlide);
+      }
+    });
+    thisHome.activeDot(thisHome.currentSlide);
+  }
+
+  activeDot(current) {
+    document.querySelectorAll('.dot').forEach((dot) => {
+      dot.classList.remove('dot--active');
+      if (Number(dot.dataset.slide) === current) {
+        dot.classList.add('dot--active');
+      }
+    });
+  }
+
+  goToSlide(current) {
+    const thisHome = this;
+    thisHome.slides.forEach((slide, index) => {
+      slide.style.transform = `translateX(${(index - current) * 100}%)`;
+    });
   }
 }
 
